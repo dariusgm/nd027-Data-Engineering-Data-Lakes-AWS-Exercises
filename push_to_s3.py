bc823d39389efc180a3d926de8f86f137092b98d
Darius Murawski <dariusmurawski@hotmail.com>
2024-03-17 14:06:33 +0100

    add glue db creation

--- push_to_s3.py
+++ push_to_s3.py
@@ -3,9 +3,10 @@ import os.path
 
 import boto3
 
-
 # Inspired by: https://stackoverflow.com/questions/4103773/efficient-way-of-having-a-function-only-execute-once-in-a-loop
 from functools import wraps
+
+
 def create_bucket_if_not_exists(bucket_name):
     """
     create a bucket when it not exists with the name "bucket_name"
@@ -19,7 +20,6 @@ def create_bucket_if_not_exists(bucket_name):
     s3.create_bucket(Bucket=bucket_name)
 
 
-
 def copy_data(bucket_name):
     """
     copy the data from project/starter/* to the newly created bucket
@@ -29,20 +29,89 @@ def copy_data(bucket_name):
         if os.path.isdir(file):
             continue
         filename = file.split('/')[-1]
-        print(f'Copying {file} to s3://{bucket_name}/{filename}')
-        s3_client.upload_file(file, bucket_name, filename)
+        if "accelerometer" in filename:
+            full_filename = f"accelerometer/landing/{filename}"
+        elif "customer" in filename:
+            full_filename = f"customer/landing/{filename}"
+        elif "step_trainer" in filename:
+            full_filename = f"step_trainer/landing/{filename}"
+        else:
+            continue
+        print(f'Copying {file} to s3://{bucket_name}/{full_filename}')
+        s3_client.upload_file(file, bucket_name, full_filename)
+
 
 def touch(marker: str):
     with(open(f"marker_{marker}.txt", 'wt')) as m:
         m.write("done")
 
+
 def exists(marker: str):
     return os.path.exists(f"marker_{marker}.txt")
 
 
-def create_customer_landing(bucket_name):
-    glue = boto3.client('glue')
-    # create a table with the name "customer_landing" in glue
+def create_database():
+    glue = boto3.client('glue', region_name='us-east-1')
+
+    for db in glue.get_databases()['DatabaseList']:
+        if db['Name'] == 's3db':
+            print('Database s3db already exists')
+            return
+
+    # create a database with the name "s3db" in glue
+    response = glue.create_database(
+        DatabaseInput={
+            'Name': 's3db'
+        }
+    )
+
+def create_accelerometer_landing(bucket_name):
+    glue = boto3.client('glue', region_name='us-east-1')
+
+    # create a table with the name "accelerometer" in glue
+    # We have 3 column:
+    # "user" is a "string",
+    # "timestamp" is a "bigint",
+    # "x" is a "double"
+    # "y" is a "double"
+    # "z" is a "double"
+    # the data is stored in json format
+    response = glue.create_table(
+        DatabaseName='s3db',
+        TableInput={
+            'Name': 'accelerometer',
+            'StorageDescriptor': {
+                'Columns': [
+                    {
+                        'Name': 'user',
+                        'Type': 'string'
+                    },
+                    {
+                        'Name': 'timestamp',
+                        'Type': 'bigint'
+                    },
+                    {
+                        'Name': 'x',
+                        'Type': 'double'
+                    },
+                    {
+                        'Name': 'y',
+                        'Type': 'double'
+                    },
+                    {
+                        'Name': 'z',
+                        'Type': 'double'
+                    }
+                ],
+                'Location': f's3://{bucket_name}/accelerometer/landing/',
+                'InputFormat': 'org.apache.hadoop.mapred.TextInputFormat',
+                'OutputFormat': 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat',
+                'SerdeInfo': {
+                    'SerializationLibrary': 'org.openx.data.jsonserde.JsonSerDe'
+                }
+            }
+        }
+    )
 
 
 
@@ -59,9 +128,14 @@ def main():
         copy_data(bucket_name)
         touch(step2)
 
-    step3 = "create_customer_landing"
+    step3 = "create_database"
     if not exists(step3):
-        create_customer_landing(bucket_name)
+        create_database()
+        touch(step3)
+
+    step4 = "create_accelerometer_landing"
+    if not exists(step4):
+        create_accelerometer_landing(bucket_name)
         touch(step3)
 
 
